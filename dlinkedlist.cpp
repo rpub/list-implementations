49f5cfe62159ff7c7acabf67158b3af5f9f7e881
Rami <isaacr@spu.edu>
2020-05-14 23:57:53 -0700

    Final
    Complete

--- dlinkedlist.cpp
+++ dlinkedlist.cpp
@@ -1,47 +1,291 @@
-// Remember to write your headers!
-
-
+/*
+ * Title:		Doubly Linked List, Methods
+ * Purpose:		Method implementations
+ * Author:		Carlos Arias/Rami Isaac
+ * Date:		May 14, 2020
+ */
 #include "object.h"
 #include "dlinkedlist.h"
 
 #include <string>
-using std::string;
+#include <iostream>
+#include <sstream>
 
-DoubleLinkedList::DoubleLinkedList(const DoubleLinkedList &other){
+// using namespace std;
+using std::string;
+using std::istream;
+using std::ostream;
+using std::stringstream;
+
+
+/**
+ * Default constructor
+ * Just sets the head and tail to nullptr
+ */
+DoubleLinkedList::DoubleLinkedList() : _head(nullptr), _tail(nullptr){}
+
+/**
+ * Copy Constructor
+ * Creates a deep copy of the list. It creates a clone each of the
+ * elements in the original list and inserts them into the copy,
+ * this insertion is done using the Insert method.
+ * @param DoubleLinkedList &other
+ */
+DoubleLinkedList::DoubleLinkedList(const DoubleLinkedList &other) : _head(nullptr){
+    for (Node* tmp = other._head; tmp != nullptr; tmp = tmp->next){
+        Insert(tmp->data->Clone(), _size);
+    }
 }
 
+/**
+ * Copy assignment operator
+ * Enables the deep copy assignment using the operator = overload. This
+ * method is kept private to avoid usage from clients of the class.
+ * This method also relies on the Clone method to create copies of the
+ * elements.
+ * @param rhs the object to be copied into this
+ * @return this to enable cascade assignments
+ */
 DoubleLinkedList &DoubleLinkedList::operator=(const DoubleLinkedList &rhs) {
+    for (Node* tmp = rhs._head; tmp != nullptr; tmp = tmp->next){
+        Insert(tmp->data->Clone(), _size);
+    } // Sets elements equal to rhs
 	return *this;
 }
 
-DoubleLinkedList::DoubleLinkedList(){
-
-}
-
+/**
+ * Destructor
+ * Frees the dynamic memory allocated for the list, it does so by
+ * traversing the list and releasing the memory of each element and
+ * then its node.
+ */
 DoubleLinkedList::~DoubleLinkedList() {
-
+    Node* tempNode = _head;
+    while (_head != nullptr){
+        _head = tempNode->next;
+        if (tempNode->data)
+            delete tempNode->data;
+        delete tempNode;
+        tempNode = _head;
+    }
 }
 
+/**
+ * Insert()
+ * Inserts an element into a given position.
+ * See comments in method below for detailed explanation.
+ * @param element what the client wants to insert into the list
+ * @param position the position where the element is to be inserted
+ * @return true if it was possible to insert, false otherwise. It will not be able
+ * to insert if the position is invalid.
+ */
 bool DoubleLinkedList::Insert(Object *element, size_t position) {
-	return false;
+
+    // If position is valid
+    if (position > _size){
+        return false;
+    }
+
+    Node* neo = new Node();  // Allocate memory for new node, 'neo'
+    neo->data = element;     // Assign data to neo
+    neo->next = nullptr;     // Next ptr, null by default
+    neo->previous = nullptr; // Previous ptr, null by default
+
+    if (_size == 0){
+        /* --- INSERT NODE AT FRONT
+         * If the list's head pointer is null (list is empty), the algorithm...
+         * 1. Points the list's head and tail pointers to the new node.
+         */
+        /* [ 1a ] */ _head = neo;
+        /* [ 1b ] */ _tail = neo;
+    } else if (position == 0) {
+        /* --- INSERT NEW, UPDATED LIST _HEAD
+         * If the list is not empty and user wishes to insert neo as head, the algoirthm...
+         * 1. Links the list
+         * 2. Assigns the 'new' neo head
+         */
+        neo->next = _head;     // Assigns next of neo to _head
+        _head->previous = neo; // Assigns previous of head to neo;
+        _head = neo;             // Assigns _head to neo
+    } else if (position == _size - 1) {
+        /* --- INSERT NODE AFTER TAIL
+         *  the list's head pointer is not null (list is not empty) and curNode
+         *  points to the list's tail node, the new node is inserted after the tail node.
+         *
+         *  The algorithm...
+         *
+         * 1. Points the tail node's next pointer to the new node
+         * 2. Points the new node's previous pointer to the list's tail node
+         * 3. Points the list's tail pointer to the new node
+         * 4. Points the new node's next pointer to null (tail node)
+         */
+        /* [ 1 ] */ _tail -> next = neo;
+        /* [ 2 ] */ neo -> previous = _tail;
+        /* [ 3 ] */ _tail = neo;
+        /* [ 4 ] */ neo -> next = nullptr;
+    } else {
+        /* --- INSERT NODE IN MIDDLE
+         * If the list's head pointer is not null (list is not empty) and curNode does
+         * not point to the list's tail node, the algorithm updates the current, new, and
+         * successor nodes' next and previous pointers to achieve the ordering
+         * {curNode newNode sucNode}, which requires four pointer updates:
+         *
+         * 1. Point the new node's next pointer to sucNode
+         * 2. Point the new node's previous pointer to curNode
+         * 3. Point curNode's next pointer to the new node
+         * 4. Point sucNode's previous pointer to the new node.
+         */
+        Node* tmp = _head; // Allocate memory for a temporary node
+
+        // Traverse through list
+        for (size_t i = 0; i < position - 1; i++){
+            tmp = tmp -> next;
+        }
+
+        /* [ 1 ] */ neo -> next = tmp -> next; // Assign next of neo equal to next of previous node
+        /* [ 2 ] */ neo -> previous = tmp;     // Assign previous of neo to previous node
+        /* [ 3 ] */ tmp -> next = neo;         // Assign next of previous node to neo
+        if(neo -> next != nullptr) // Check if neo is the tail
+            /* [ 4 ] */ neo -> next -> previous = neo; // Assign previous of node next to neo, to neo
+    }
+    _size++;
+    return true;
 }
 
+/**
+ * Index Of
+ * Searches for the position of an element in the list.
+ * The method performs a linear search, traversing the list, for the given
+ * element.
+ * If the element is found it returns the position, if it is not found it
+ * returns -1. It is important to note that for this operation to succeed it
+ * is necessary to override the method Equals of all the types inserted into
+ * the list.
+ * @param element the object that the client is searching for.
+ * @return the position of the element if found, -1 otherwise.
+ */
 int DoubleLinkedList::IndexOf(const Object *element) const {
-	return -1;
+    size_t index = 0;
+    for (Node* tmp = _head; tmp != nullptr; tmp = tmp->next){
+        if (tmp->data->Equals(*element)){
+            return index;
+        }
+        index++;
+    }
+    return -1;
 }
 
+/**
+ * Remove()
+ * Removes the element at position, when the position is valid. It returns
+ * the pointer to the removed element. This method does not release any
+ * dynamically allocated object. It is left to the client to release the
+ * memory of the removed element. After removing the element, the list needs
+ * to be correctly link to avoid losing the reference to the elements after
+ * the removed element.
+ * @param position the position of the element to be removed.
+ * @return the pointer to the object in that position if the position was valid,
+ * nullptr otherwise
+ */
 Object *DoubleLinkedList::Remove(size_t position) {
-	return nullptr;
+
+    // If position is valid
+    if (position >= _size) {
+        return nullptr;
+    }
+
+    Node* tmp = nullptr;
+    Object* retVal = nullptr;
+
+    if(position == 0){
+        retVal = _head->data;
+        _head->data = nullptr;
+        tmp = _head->next;
+        _head->next = nullptr;
+        if(_tail == _head){
+            _tail = tmp;
+        }
+        delete _head;
+        _head = tmp;
+    } else {
+
+        tmp = _head; // Traverse list
+        for (size_t i = 0; i < position -1; i++){
+            tmp = tmp->next;
+        }
+
+        Node* toRemove = tmp->next; // New node is going to be after current node
+        retVal = toRemove->data;    // Holds value of new node
+        tmp->next = toRemove->next;
+
+        // Determines if tail / middle node
+        if(toRemove->next != nullptr){
+            toRemove->next->previous = tmp;
+            toRemove->next = nullptr; // Tails
+        } else { //element removed is at end of list
+            _tail = tmp;
+        }
+        toRemove->previous = nullptr; // Clears original node
+        toRemove->data = nullptr;     // Clears original node
+        delete toRemove;              // Deletes original node
+    }
+    _size--;
+    return retVal;
 }
 
+/**
+ * Get Position
+ * Returns the element at a given position. Note that it is not a copy of the
+ * element.
+ * @param position the position of the element to retrieve.
+ * @return a pointer to the element if the position is valid, nullptr otherwise
+ */
 Object *DoubleLinkedList::Get(size_t position) const {
-	return nullptr;
+    if (position >= _size){
+        return nullptr;
+    }
+    Node* tmp = _head;
+    for (size_t i = 0; i < position; i++) {
+        tmp = tmp->next;
+    }
+    return tmp->data;
 }
 
+/**
+ * ToString()
+ * Creates a string representation of the list. This representation
+ * will be the string representation of each of the elements, surrounding
+ * the whole list with curly braces and separating the elements by comma.
+ * Example: {2, 6, 8}
+ * @return a string representation of the list
+ */
 string DoubleLinkedList::ToString() const {
-	return "";
+    stringstream retVal;
+    retVal << "{";
+    Node* tmp;
+    for (tmp = _head; tmp->next != nullptr; tmp = tmp->next) {
+        retVal << tmp->data->ToString() << ", ";
+    }
+    retVal << tmp->data->ToString() << "}";
+    return retVal.str();
 }
 
+/**
+ * Clear()
+ * Very similar to the destructor, this method releases all the elements
+ * of the list. It also sets the size to zero, and the head to nullptr.
+ * This method makes the list become empty.
+ */
 void DoubleLinkedList::Clear() {
-
+    Node* tempNode = _head;
+    while (_head != nullptr){
+        _head = tempNode->next;
+        if (tempNode->data)
+            delete tempNode->data;
+        delete tempNode;
+        tempNode = _head;
+    }
+    _head = nullptr;
+    _tail = nullptr;
+    _size = 0;
 }
--- vsarray.cpp
+++ vsarray.cpp
@@ -1,47 +1,258 @@
-//
-// Created by Arias Arevalo, Carlos on 5/6/20.
-//
+/*
+ * Title:		Variable Size Array List
+ * Purpose:		Implementation the ADT List using an underlying variable size array.
+ * 				The array will be allocated at construction with the specified
+ * 				capacity.
+ * Author:		Carlos Arias/Rami Isaac
+ * Date:		May 14, 2020
+ */
 
 #include "object.h"
 #include "list.h"
 #include "vsarray.h"
 
 #include <string>
+#include <iostream>
+#include <sstream>
+
+// using namespace std;
 using std::string;
+using std::istream;
+using std::ostream;
+using std::stringstream;
 
-VSArray::VSArray(size_t capacity, double increasePercentage){
+/**
+ * Constructor with capacity
+ * Note: This constructor should be made explicit to avoid auto-conversions
+ * Given a capacity the constructor allocates an array to Object*
+ * this array will later hold pointers to the actual elements
+ * that are being inserted into the list. The constructor also initializes
+ * the elements of the array to nullptr
+ * @param capacity the number of elements that the list is able to hold
+ * @param delta the percentage change
+ */
+VSArray::VSArray(size_t capacity, double increasePercentage) : _capacity(capacity), _delta(increasePercentage){
+    _data = new Object*[_capacity];
+    for (size_t i = 0; i < _capacity; i++) {
+        _data[i] = nullptr;
+    }
 }
-VSArray::VSArray(const VSArray& list){
+
+/**
+ * Copy Constructor
+ * Creates a deep copy of the list. It allocates the same capacity of
+ * elements for the copy and then clones each of the elements in the
+ * original list and inserts them into the copy, this insertion is
+ * done directly into the array.
+ * @param list the list to be copied
+ */
+VSArray::VSArray(const VSArray& list) : _capacity(list._capacity){
+    _size = list._size;
+    _data = new Object*[_capacity];
+    for (size_t i = 0; i < _size; i++) {
+        _data[i] = list.Get(i)->Clone();
+    }
+    for (size_t i = _size; i < _capacity; i++){
+        _data[i] = nullptr;
+    }
 }
+
+/**
+ * Destructor
+ * It frees the dynamic memory allocated for the list, it does so in
+ * two steps:
+ * 1. Traverses the list and releases the memory of each element. This in
+ *    turn will call the destructor of each of the elements.
+ * 2. Once the elements have been freed, then the array memory is released.
+ */
 VSArray::~VSArray(){
+    for (size_t i = 0; i < _size; i++) {
+        delete _data[i];
+    }
+    delete[] _data;
 }
+
+/**
+ * Copy assignment operator
+ * Enables the deep copy assignment using the operator = overload. This
+ * method is kept private to avoid usage from clients of the class.
+ * This method also relies on the Clone method to create copies of the
+ * elements.
+ * Note: Should the parameter be const List& ?
+ * @param rhs the object to be copied into this
+ * @return this to enable cascade assignments
+ */
 const VSArray& VSArray::operator=(const VSArray& rhs){
-	return *this;
+    if (&rhs == this){
+        return *this;
+    }
+    if (_capacity != rhs._capacity){
+        Clear();
+        delete[] _data;
+        _capacity = rhs._capacity;
+        _data = new Object*[_capacity];
+        for (size_t i = 0; i < _capacity; i++) {
+            _data[i] = nullptr;
+        }
+    }
+    for (size_t i = 0; i < rhs.Size(); i++) {
+        _data[i] = rhs.Get(i)->Clone();
+    }
+    return *this;
 }
 
+/**
+ * Inserts an element into a given position.
+ * This operation has the following steps:
+ * 1. Check if the position is valid
+ * 2. Check if there is enough space
+ * 3. If the position is not the last, it needs to push the elements
+ * 4. Insert the element in the given position
+ * 5. Increase the size
+ * @param element what the client wants to insert into the list
+ * @param position the position where the element is to be inserted
+ * @return true if it was possible to insert, false otherwise. It will not be able
+ * to insert if the position is invalid or if the list is full.
+ */
 bool VSArray::Insert(Object* element, size_t position){
-	return false;
+
+    // Check if the position is valid
+    if (position > _size)
+        return false;
+
+    // Check if there is enough space
+    if (_capacity == _size)
+        Resize();
+
+    if (position < _size){ // We need to shift
+        // Be careful with size_t when subtracting
+        for (size_t i = _size; i > position; i--){
+            _data[i] = _data[i - 1];
+        }
+    }
+    _data[position] = element;
+    _size++;
+    return true;
 }
+
+/**
+ * Searches for the position of an element in the list.
+ * The method performs a linear search on the array for the given element.
+ * If the element is found it returns the position, if it is not found it
+ * returns -1. It is important to note that for this operation to succeed it
+ * is necessary to override the method Equals of all the types inserted into
+ * the list.
+ * @param element the object that the client is searching for.
+ * @return the position of the element if found, -1 otherwise.
+ */
 int VSArray::IndexOf(const Object* element)const{
-	return -1;
+    for (size_t i = 0; i < _size; i++) {
+        if (_data[i]->Equals(*element)){
+            return i;
+        }
+    }
+    return -1;
 }
+
+/**
+ * Removes the element at position, when the position is valid. It returns
+ * the pointer to the removed element. This method does not release any
+ * dynamically allocated object. It is left to the client to release the
+ * memory of the removed element. After removing the element, the rest of
+ * the elements in the list will be pushed to cover the empty space, unless
+ * the removed element is the last, in which case is just set to nullptr.
+ * @param position the position of the element to be removed.
+ * @return the pointer to the object in that position if the position was valid,
+ * nullptr otherwise
+ */
 Object* VSArray::Remove(size_t position){
-	return nullptr;
+    if (position >= _size){
+        return nullptr;
+    }
+    Object* retVal = _data[position];
+    for (size_t i = position; i < _size - 1; i++) {
+        _data[i] = _data[i + 1];
+    }
+    _data[_size - 1] = nullptr;
+    _size--;
+    return retVal;
+	//return nullptr; // original return value
 }
+
+/**
+ * Element Access
+ * Returns the element at a given position. Note that it is not a copy of the
+ * element.
+ * @param position the position of the element to retrieve.
+ * @return a pointer to the element if the position is valid, nullptr otherwise
+ */
 Object* VSArray::Get(size_t position)const{
-	return nullptr;
+    if (position >= _size)
+        return nullptr;
+    else
+        return _data[position];
 }
+
+/**
+ * Creates a string representation of the list. This representation
+ * will be the string representation of each of the elements, surrounding
+ * the whole list with curly braces and separating the elements by comma.
+ * Example: {2, 6, 8}
+ * @return a string representation of the list
+ */
 string VSArray::ToString()const{
-	return "";
+    stringstream retVal;
+    retVal << "{";
+    for (size_t i = 0; i < _size - 1; i++) {
+        retVal << _data[i]->ToString() << ", ";
+    }
+    retVal << _data[_size - 1]->ToString() << "}";
+    return retVal.str();
 }
+
+/**
+ * Very similar to the destructor, this method releases all the elements
+ * of the list, but it does not release the array to hold elements. It also
+ * sets the size to zero. This method makes the list become empty.
+ */
 void VSArray::Clear(){
-
+    for (size_t i = 0; i < _size; i++) {
+        delete _data[i];
+        _data[i] = nullptr;
+    }
+    _size = 0;
 }
 
+/**
+ * Capacity accessor
+ * @return the number of elements the list is able to hold
+ */
 void VSArray::Resize() {
+    // (1) Calculates capacity of resized array
+    size_t higherCapacity = static_cast<size_t>(_capacity * (_delta + 1));
 
+    // (2) Allocate new array w/ size of new capacity
+    Object** tempData = new Object*[higherCapacity];
+
+    // (3) Assign points of original _data[i] to tempData[i]
+    for (size_t i = 0; i < _size; i++) {
+        tempData[i] = _data[i];
+    }
+
+    // (4) Deallocate old array (DOUBLE CHECK) -----------------------------!
+    for (size_t i = _size; i < higherCapacity; i++) {
+        tempData[i] = nullptr;
+    }
+
+    delete[] _data;             // Delete data from original array
+    _data = tempData;           // Assign tempData to original array
+    _capacity = higherCapacity; // Assigned higher capacity to original array
 }
 
+/**
+ * Capacity accessor
+ * @return the number of elements the list is able to hold
+ */
 size_t VSArray::GetCapacity() const {
 	return _capacity;
 }
